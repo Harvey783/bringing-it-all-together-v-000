1f8baa54f00792004f15e1bc786488186c28d109
Chris Harvey <chharvey@icloud.com>
2017-12-19 22:13:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,8 @@
-require_relative "../config/environment.rb"
-
 class Dog
   attr_accessor :name, :breed, :id
 
   def initialize(name:, breed:, id: nil)
-    @name = name
-    @breed = breed
-    @id = id
+    @name, @breed, @id = name, breed, id
   end
 
   def self.create_table
@@ -27,16 +23,25 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, name, breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
   def self.create(name:, breed:)
     new_dog = Dog.new(name: name, breed: breed)
     new_dog.save
     new_dog
   end
-
-  def self.new_from_db(row)
-    Dog.new(id: row[0], name: row[1], breed: row[2])
-  end
-
 
   def self.find_by_id(id)
     sql = <<-SQL
@@ -48,4 +53,34 @@ class Dog
       self.new_from_db(row)
     end.first
   end
-end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name = ?;
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
