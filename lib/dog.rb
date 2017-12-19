5d2ab7c9a730b220b8ab65ff82aed77a6789a162
Chris Harvey <chharvey@icloud.com>
2017-12-19 21:33:21 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+require_relative "../config/environment.rb"
+
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name, breed, id=nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs {
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    }
+    SQL
+    DB[:conn].execute(sql)
+  end
+end
